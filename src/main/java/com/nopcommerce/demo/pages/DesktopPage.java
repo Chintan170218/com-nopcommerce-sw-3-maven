792e5dd079bded8ada9f4361442c62b3a2c1f968
Chintan170218 <chintantiwari7@gmail.com>
2022-05-04 00:47:02 +0100

    com-nopcommerce-sw-3-maven

---
+++ src/main/java/com/nopcommerce/demo/pages/DesktopPage.java
@@ -0,0 +1,30 @@
+package com.nopcommerce.demo.pages;
+
+import com.nopcommerce.demo.utility.Utility;
+import org.openqa.selenium.By;
+
+public class DesktopPage extends Utility {
+
+    By zToA = By.id("products-orderby");
+    By zToAText = By.xpath("//select[@id='products-orderby']//option[3]");
+    By aToZ = By.id("products-orderby");
+    By buildYourComputer = By.xpath("//button[@onclick='return AjaxCart.addproducttocart_catalog" +
+            "(\"/addproducttocart/catalog/1/1/1\"),!1']");
+
+
+    public void selectZtoA(){
+        selectByVisibleTextFromDropDown(zToA, "Price: Low to High");
+    }
+
+    public String getZtoAText(){
+        return getTextFromElement(zToAText);
+    }
+
+    public void selectAtoZ(){
+        selectByVisibleTextFromDropDown(aToZ, "Name: A to Z");
+    }
+
+    public void selectBuildYourComputer() {
+        mouseHoverToElementAndClick(buildYourComputer);
+    }
+}
